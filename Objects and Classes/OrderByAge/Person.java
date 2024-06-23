c4a111ff6dbe8dd09ce7bbd92e7d572e7497dc2a
Borislav_Angelov <borislavangeloff@gmail.com>
2024-06-24 02:01:12 +0300

    Add files via upload

---
+++ Objects and Classes/OrderByAge/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+
+    // 1. Fields
+    private String name;
+    private String id;
+    private int age;
+
+    // 2. Constructor
+    public Person(String name, String id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    // 3. Methods
+    public String getName() {
+        return this.name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
